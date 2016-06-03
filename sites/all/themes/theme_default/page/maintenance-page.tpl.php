81f5e42af41970bc9dbd412ed2d7c9afe4b6be75
nguyenduypt86 <nguyenduypt86@gmail.com>
2016-06-03 21:01:12 +0700

    duy

--- sites/all/themes/theme_default/page/maintenance-page.tpl.php
+++ sites/all/themes/theme_default/page/maintenance-page.tpl.php
@@ -1,11 +1,12 @@
 <?php global $base_url; ?>
-<div class="wrap-maintenance">
-	<img src="<?php echo $base_url ."/".path_to_theme()?>/img/maintenance.png" />
-</div>
-
+<div class="wrap-maintenance">Trang web đang bảo trì.</div>
 <style>
 	div.wrap-maintenance {
     	margin-top: 5%;
     	text-align: center;
+    	font-size: 20px;
+    	font-style: italic;
+    	color: #555;
+    	line-height: 100px;
 	}
 </style>
